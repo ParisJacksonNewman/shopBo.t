4291e6e826cf4f47f65b94ca3084d329d6a9e4e2
Paris Jackson-Newman <jackson.pnew@gmail.com>
2022-11-10 00:23:55 +0000

    Removed the cookie shit
    There was probably a better way to do this. It was long. For some reason clicking accept cookies like a regular button didn't work so I had to enable a chrome extension to block it

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 # TODO Be able to add necessary arguments
 # TODO Access the sneakers website using selenium
@@ -14,6 +15,7 @@ from selenium.webdriver.common.by import By
 
 URL = "https://www.nike.com/gb/launch"
 
+# Function to handle command line interface
 def get_args():
     parser = argparse.ArgumentParser(
         prog='Sneaky Bot',
@@ -28,17 +30,19 @@ def get_args():
     
     return args.username, args.password
 
+# Function to log into the account
 def login(username, password, url):
+
+    chrome_option = Options()
+    chrome_option.add_extension('3.4.4_0.crx')
+
     service = Service(executable_path='chromedriver')
 
-    driver = webdriver.Chrome(service=service)
-
+    driver = webdriver.Chrome(service=service, chrome_options=chrome_option)
     driver.get(url)
-
-    driver.implicitly_wait(0.5)
+    driver.implicitly_wait(3)
 
     login_button = driver.find_element(by=By.CLASS_NAME, value='join-log-in')
-
     login_button.click()
 
     driver.quit()
@@ -46,6 +50,8 @@ def login(username, password, url):
     #...
 
 username, password = get_args()
+
+login(username, password, URL)
 
 print(username)
 print(password)
